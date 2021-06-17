ac847798acb80d998e0f11d695cad393ab59c46e
srikar <jagannadhsrikar@gmail.com>
2021-06-17 23:13:35 +0530

    Updated

--- query1.sql
+++ query1.sql
@@ -1,1 +1,1 @@
-SELECT b.title,p.name from books b, publisher p WHERE p.id=b.publisher_id AND p.name='PHI';
+SELECT b.title from books b, publisher p WHERE p.id=b.publisher_id AND p.name='PHI';
--- query4.sql
+++ query4.sql
@@ -1,1 +1,1 @@
-SELECT b.title,s.name from books b, subjects s, books_subjects bs WHERE bs.books_id=b.id AND bs.subjects_id=s.id AND b.title='Atomic Habits'
+SELECT s.name from books b, subjects s, books_subjects bs WHERE bs.books_id=b.id AND bs.subjects_id=s.id AND b.title='Atomic Habits'
